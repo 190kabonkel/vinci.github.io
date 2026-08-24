9c4888b22520e66c00f68280320bde5482955bd0
vinci <joelplopkoek@gmail.com>
2026-08-24 11:37:16 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,13 +1,18 @@
-let time = 5 * 24 * 60 * 60;
+let end = localStorage.getItem("timerEnd");
+
+if (!end) {
+    end = Date.now() + 5 * 24 * 60 * 60 * 1000;
+    localStorage.setItem("timerEnd", end);
+}
 
 setInterval(() => {
-    let d = Math.floor(time / 86400);
-    let h = Math.floor(time % 86400 / 3600);
-    let m = Math.floor(time % 3600 / 60);
-    let s = time % 60;
+    let time = Math.max(0, end - Date.now());
+
+    let d = Math.floor(time / 86400000);
+    let h = Math.floor(time / 3600000) % 24;
+    let m = Math.floor(time / 60000) % 60;
+    let s = Math.floor(time / 1000) % 60;
 
     document.getElementById("timer").textContent =
         `${d}d ${h}h ${m}m ${s}s`;
-
-    time--;
 }, 1000);
